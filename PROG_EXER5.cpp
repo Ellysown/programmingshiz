4337d9306a57c3ad8be0aa90bf99b4a8a97d00c4
Elly <41644174+Ellysown@users.noreply.github.com>
2018-09-30 21:18:51 +0800

    Add files via upload

---
+++ PROG_EXER5.cpp
@@ -0,0 +1,19 @@
+/*
+*NAME: John Ellyson
+*DATE: September 01, 2018
+*EXERCISE NAME: Conditional Ohms
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+float i=0,v=0,r=0;
+i=0.00035;
+r=5350;
+if (i==0.00035) && (r==5350) {
+v=i*r;
+cout<<v;
+}
+return 0;
+}
